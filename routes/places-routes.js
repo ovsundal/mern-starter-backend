7d48d5695ceb8c75123f5a86da7cb0d1f329b028
osund <osundal83@gmail.com>
2020-02-18 21:49:16 +0100

    fix endpoint: Use filter instead of search

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get('/:pid', placesControllers.getPlaceById);
 
-router.get('/user/:userId', placesControllers.getPlaceByUserId);
+router.get('/user/:userId', placesControllers.getPlacesByUserId);
 
 router.post('/', placesControllers.createPlace);
 
