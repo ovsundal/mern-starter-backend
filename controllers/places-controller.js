54c25b2d3b2d6727efdc4875b404b27cae798f93
ove <osundal83@gmail.com>
2020-02-20 16:36:37 +0100

    connected to google api for coordinates

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -1,6 +1,7 @@
 const HttpError = require('../models/http-error');
 const uuid = require('uuid/v4');
 const {validationResult} = require('express-validator');
+const getCoordsForAddress = require('../util/location');
 
 let DUMMY_PLACES = [
     {
@@ -45,13 +46,21 @@ const getPlacesByUserId = (req, res, next) => {
     res.json({place: places});
 };
 
-const createPlace = (req, res, next) => {
+const createPlace = async (req, res, next) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
-        throw new HttpError('Invalid inputs passed, please check your data.', 422);
+        return next(new HttpError('Invalid inputs passed, please check your data.', 422));
     }
 
-    const {title, description, coordinates, address, creator} = req.body;
+    const {title, description, address, creator} = req.body;
+
+    let coordinates;
+
+    try {
+        coordinates = await getCoordsForAddress(address);
+    } catch(error) {
+        return next(error);
+    }
 
     const createdPlace = {
         id: uuid(),
