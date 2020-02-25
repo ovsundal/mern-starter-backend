62090b171cfe5a6932edb8cacb1eead7845c195a
osund <osundal83@gmail.com>
2020-02-25 21:06:16 +0100

    replace dummy login logic

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -14,8 +14,18 @@ const DUMMY_USERS = [
     }
 ];
 
-const getUsers = (req, res, next) => {
-    res.json({users: DUMMY_USERS});
+const getUsers = async (req, res, next) => {
+    let users;
+    try {
+        users = await User.find({}, '-password');
+    }
+    catch (err) {
+        const error = new HttpError(
+            'Fetching users failed, please try again later', 500
+        );
+        return next(error);
+    }
+    res.json({users: users.map((user) => user.toObject({getters: true}))});
 };
 
 const signup = async (req, res, next) => {
@@ -26,7 +36,7 @@ const signup = async (req, res, next) => {
         );
     }
 
-    const {name, email, password, places} = req.body;
+    const {name, email, password} = req.body;
 
     let existingUser;
     try {
@@ -55,7 +65,7 @@ const signup = async (req, res, next) => {
             '\n' +
             'C/O https://placeholder.com/ ',
         password,
-        places
+        places: []
     });
 
     try {
@@ -71,12 +81,24 @@ const signup = async (req, res, next) => {
     res.status(201).json({user: createdUser.toObject({getters: true})});
 };
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
     const {email, password} = req.body;
-    const identifiedUser = DUMMY_USERS.find(u => u.email === email);
 
-    if(!identifiedUser || identifiedUser.password !== password) {
-        throw new HttpError('Could not identify user, credentials seem to be wrong', 401);
+    let existingUser;
+    try {
+        existingUser = await User.findOne({email});
+    }catch (e) {
+        const error = new HttpError(
+            'Logging in failed, please try again later.', 500
+        );
+        return next(error);
+    }
+
+    if(!existingUser || existingUser.password !== password) {
+        const error = new HttpError(
+            'Invalid credentials, could not log you in.', 401
+        );
+        return next(error);
     }
 
     res.json({message: 'Logged in!'})
