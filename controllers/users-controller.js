17352a9fed98d4464b5f22ecfcfbb245dfae2518
osund <osundal83@gmail.com>
2020-03-03 21:36:02 +0100

    hash user password

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,8 +1,8 @@
 const {validationResult} = require("express-validator");
-
 const HttpError = require('../models/http-error');
-
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const getUsers = async (req, res, next) => {
     let users;
@@ -47,12 +47,19 @@ const signup = async (req, res, next) => {
 
     }
 
+    let hashedPassword;
+    try {
+        hashedPassword = await bcrypt.hash(password, 12);
+    } catch(err) {
+        const error = new HttpError('Could not create user, please try again.', 500);
+        return next(error);
+    }
 
     const createdUser = new User({
         name,
         email,
         image: 'http://localhost:5000/' + req.file.path,
-        password,
+        password: hashedPassword,
         places: []
     });
 
@@ -82,12 +89,27 @@ const login = async (req, res, next) => {
         return next(error);
     }
 
-    if(!existingUser || existingUser.password !== password) {
+    if(!existingUser) {
         const error = new HttpError(
             'Invalid credentials, could not log you in.', 401
         );
         return next(error);
     }
+
+    let isValidPassword = false;
+    try {
+        isValidPassword = await bcrypt.compare(password, existingUser.password);
+    } catch(e) {
+        const error = new HttpError('Could not log you in, please check your credentials and try again', 500);
+        return next(error);
+    }
+
+    if(!isValidPassword) {
+        const error = new HttpError('Could not log you in, please check your credentials and try again', 401);
+
+        return next(error);
+    }
+
 
     res.json({message: 'Logged in!', user: existingUser.toObject({getters: true})})
 };
