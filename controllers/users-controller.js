48e45e202fac5c82057730d7e7a41e639cdccd50
osund <osundal83@gmail.com>
2020-03-03 21:46:50 +0100

    add web token during authentication

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -73,7 +73,19 @@ const signup = async (req, res, next) => {
         return next(error);
     }
 
-    res.status(201).json({user: createdUser.toObject({getters: true})});
+    let token;
+
+    try {
+        token = jwt.sign({userId: createdUser.id, email: createdUser.email}, 'supersecret_dont_share', {expiresIn: '1h'});
+    } catch(e) {
+        const error = new HttpError(
+            'Signing up failed, please try again',
+            500
+        );
+        return next(error);
+    }
+
+    res.status(201).json({userId: createdUser.id, email: createdUser.email, token});
 };
 
 const login = async (req, res, next) => {
@@ -110,8 +122,23 @@ const login = async (req, res, next) => {
         return next(error);
     }
 
+    let token;
+    try {
+        token = jwt.sign({userId: existingUser.id, email: existingUser.email}, 'supersecret_dont_share', {expiresIn: '1h'});
+    } catch(e) {
+        const error = new HttpError(
+            'Loggin in failed, please try again',
+            500
+        );
+        return next(error);
+    }
 
-    res.json({message: 'Logged in!', user: existingUser.toObject({getters: true})})
+
+    res.json({
+        userId: existingUser.id,
+        email: existingUser.email,
+        token
+    })
 };
 
 exports.getUsers = getUsers;
